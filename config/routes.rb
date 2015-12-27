33586ec3b611e453252d6388d06f04dede2022f3
bernard laveaux <blaveaux@outlook.com>
2015-12-26 21:09:31 -0500

    restructed api removing API subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ require 'api_constraints.rb'
 MarketPlaceApi::Application.routes.draw do
   devise_for :users
   # Api definition
-  namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
-      resources :users, :only => [:show]
-    end
+#  namespace :api, defaults: { format: :json },
+#                              constraints: { subdomain: 'api' }, path: '/'  do
+
+  scope module: :v1,
+        constraints: ApiConstraints.new(version: 1, default: true) do
+    # We are going to list our resources here
+    resources :users, :only => [:show]
   end
+
+#  end
 end
